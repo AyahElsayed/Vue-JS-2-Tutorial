220d7af33050cc0de9e6405f085b4c10f2346784
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-05 16:47:46 +0200

    Intro to Components

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-var one = new Vue({
-    el: '#vue-app-one',
-    data: {
-      title: 'Vue App One'
-    },
-    computed: {
-      greet: function(){
-        return 'Hello, from app one :)';
-      }
-    }
-});
-
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-      title: 'Vue App Two'
-    },
-    computed: {
-      greet: function(){
-        return 'Yo dudes, this is app 2 speaking to ya';
-      }
+Vue.component('greeting', {
+    template: '<p>Hey there, I am {{name}} <button v-on:click="changName">change name</button></p>',
+    data: function () {
+        return {
+            name: 'Aya'
+        }
     },
     methods: {
-      changeTitle: function(){
-        one.title = 'Title Changed';
-      }
-    }
+        changName: function () {
+            this.name = 'Mai'
+        }
+    },
 });
 
-two.title = 'Changed from outside';
+
+
+new Vue({
+    el: '#vue-app-one'
+});
+
+new Vue({
+    el: '#vue-app-two'
+});
